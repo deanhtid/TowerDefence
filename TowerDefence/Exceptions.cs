4c342bb0bc298cb58428f00c006081bbd9f8a120
Dean Whittaker <dwhittaker@northerngas.co.uk>
2016-11-24 09:01:07 +0000

    Add project files.

---
+++ TowerDefence/Exceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefence
+{
+    class TowerDefenceException : Exception
+    {
+        public TowerDefenceException()
+        {
+
+        }
+        public TowerDefenceException(string message) : base(message)
+        {
+
+        }
+    }
+
+    class OutOfBoundsException : TowerDefenceException
+    {
+        public OutOfBoundsException()
+        {
+
+        }
+        public OutOfBoundsException(string message) : base(message)
+        {
+
+        }
+    }
+}
